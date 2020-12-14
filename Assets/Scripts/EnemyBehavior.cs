0dd55e2bc3b7283c0c86a7d073f0eb1c508b27dd
bslease <bslease@gmail.com>
2020-12-14 12:44:08 -0700

    chapter 7 complete

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public bool demoKinematicMovement = false;
+
+    private float vInput;
+    private float hInput;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        if (demoKinematicMovement)
+        {
+            // NOTE: with default rigidbody settings, player will sometimes spin in place
+            // to fix, increase angular drag on rigidbody from 0.05 to, like, 100
+            MoveKinematically();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (demoKinematicMovement)
+        {
+            return;
+        }
+
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+
+    void MoveKinematically()
+    {
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+}
